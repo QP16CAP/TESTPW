bca80424135bb303938b1ed126e4d4c5251a00eb
QP16CAP <taibi.massinissa2@gmail.com>
2026-01-19 11:48:34 +0100

    modifier le 1 fichier pour creer un conflit

--- test.ts
+++ test.ts
@@ -1,3 +1,18 @@
 //commentaire
 let message: string = "Bonjour le monde!";
 console.log(message);
+//ajout d'un commentaire
+//fin du code
+//autre commentaire
+//dernier commentaire
+//commentaire final
+
+//ajout code
+let nombre: number = 42;
+console.log("Le nombre est:", nombre);
+
+//fin ajout code
+
+//commentaire supplémentaire
+//fin du fichier
+
